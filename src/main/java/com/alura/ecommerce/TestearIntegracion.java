29e1e3b19b2e001a4bb851bc8eff043acfda226a
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2025-08-17 17:15:06 -0300

    Initial commit

---
+++ src/main/java/com/alura/ecommerce/TestearIntegracion.java
@@ -0,0 +1,8 @@
+package com.alura.ecommerce;
+
+public class TestearIntegracion {
+    public static void main(String[] args) {
+        var system = "Sos una IA que genera productos para un ecommerce. La lista de productos debe contener solamente los nombres de los productos.";
+        var usuario = "Genera 5 productos";
+    }
+}
