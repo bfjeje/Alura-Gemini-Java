43c23637bfd0e7dd65b9a72f21d6fb706b1fc758
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2025-08-17 18:47:35 -0300

    V2.3

--- src/main/java/com/alura/ecommerce/TestearIntegracion.java
+++ src/main/java/com/alura/ecommerce/TestearIntegracion.java
@@ -7,22 +7,26 @@ public class TestearIntegracion {
     public static void main(String[] args) {
         var system = "Sos una IA que genera productos para un ecommerce. La lista de productos debe contener solamente los nombres de los productos.";
         var usuario = "Genera 5 productos";
+        var APIKey = System.getenv("GEMINI_API_KEY");
 
-        Client client = new Client();
-        Content systemInstruction = Content.fromParts(Part.fromText(system));
+        try(Client client = Client.builder().apiKey(APIKey).build()){
+            Content systemInstruction = Content.fromParts(Part.fromText(system));
 
-        GenerateContentConfig config =
-                GenerateContentConfig.builder()
-                        .candidateCount(1)
-                        .systemInstruction(systemInstruction)
-                        .build();
+            GenerateContentConfig config =
+                    GenerateContentConfig.builder()
+                            .candidateCount(1)
+                            .systemInstruction(systemInstruction)
+                            .build();
 
-        GenerateContentResponse response =
-                client.models.generateContent(
-                        "gemini-2.5-flash",
-                        usuario,
-                        config);
+            GenerateContentResponse response =
+                    client.models.generateContent(
+                            "gemini-2.5-flash",
+                            usuario,
+                            config);
 
-        System.out.println(response.text());
+            System.out.println(response.text());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
