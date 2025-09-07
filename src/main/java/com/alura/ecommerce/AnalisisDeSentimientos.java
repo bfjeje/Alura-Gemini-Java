a90a44fd752ae5c785fbd3f229a77569b9dfafbf
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2025-09-07 12:21:32 -0300

    V5.2-Refactor

--- src/main/java/com/alura/ecommerce/AnalisisDeSentimientos.java
+++ src/main/java/com/alura/ecommerce/AnalisisDeSentimientos.java
@@ -6,9 +6,34 @@ import com.google.genai.types.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 public class AnalisisDeSentimientos {
     public static void main(String[] args) {
+        var archivos = cargarArchivosDeResenas();
+        for (Path archivo: archivos){
+            System.out.println("Iniciando analisis de "+archivo.getFileName().toString());
+            var respuesta = dispararRequest(archivo);
+            guardarAnalisis(archivo, respuesta);
+            System.out.println("Fin del analisis");
+        }
+    }
+
+    private static List<Path> cargarArchivosDeResenas() {
+        try {
+            var carpetaResena = Path.of("src/main/resources/resenas");
+            return Files
+                    .walk(carpetaResena, 1)
+                    .filter(path -> path.toString().endsWith(".txt"))
+                    .toList();
+        } catch (Exception e) {
+            System.out.println("Error al cargar los archivos de reseñas.");
+        }
+        return List.of();
+    }
+
+    public static String dispararRequest(Path archivo) {
+        var APIKey = System.getenv("GEMINI_API_KEY");
         var system = """
             Sos un analizador de sentimientos de reseñas de productos.
             Escribe un parrafo con hasta 50 palabras resumiendo las reseñas, y después di cual es el sentimiento general para ese producto.
@@ -21,27 +46,7 @@ public class AnalisisDeSentimientos {
             Puntos fuertes: [3 bullets points]
             Puntos debiles: [3 bullets points]
             """;
-        try {
-            var carpetaResena = Path.of("src/main/resources/resenas");
-            var archivos = Files
-                    .walk(carpetaResena, 1)
-                    .filter(path -> path.toString().endsWith(".txt"))
-                    .toList();
-            for (Path archivo: archivos){
-                System.out.println("Iniciando analisis de "+archivo.getFileName().toString());
-                var user = cargarArchivo(archivo);
-                var respuesta = dispararRequest(system, user);
-                guardarAnalisis(archivo.getFileName().toString().replace(".txt",""), respuesta);
-                System.out.println("Fin del analisis");
-            }
-        } catch (Exception e) {
-            System.out.println("Hubo un problema al analisar sentimientos");
-        }
-    }
-
-    public static String dispararRequest(String system, String user){
-        var APIKey = System.getenv("GEMINI_API_KEY");
-
+        var user = cargarArchivo(archivo);
         try(Client client = Client.builder().apiKey(APIKey).build()){
             Content systemInstruction = Content.fromParts(Part.fromText(system));
 
@@ -62,8 +67,6 @@ public class AnalisisDeSentimientos {
                             config);
 
             return response.text();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
     }
 
@@ -75,9 +78,14 @@ public class AnalisisDeSentimientos {
         }
     }
 
-    private static void guardarAnalisis(String producto, String analisis) {
+    private static void guardarAnalisis(Path archivo, String analisis) {
         try {
-            var path = Path.of("src/main/resources/analisis/analisis-sentimientos-"+producto+".txt");
+            var nombreProducto = archivo
+                    .getFileName()
+                    .toString()
+                    .replace(".txt","")
+                    .replace("resenas-","");
+            var path = Path.of("src/main/resources/analisis/analisis-sentimientos-"+nombreProducto+".txt");
             Files.writeString(path, analisis, StandardOpenOption.CREATE_NEW);
         } catch (Exception e) {
             throw new RuntimeException("Error al guardar el archivo!", e);
